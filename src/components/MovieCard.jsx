60007dd2f7ff375bb5d2bc80348be6e9dc199d8e
Abdymalik Batyrkulov <abdymalikbatyrkulov@gmail.com>
2025-06-11 21:50:18 +0300

    first commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+export const movieGenres = [
+    {
+        "id": 28,
+        "name": "Action"
+    },
+    {
+        "id": 12,
+        "name": "Adventure"
+    },
+    {
+        "id": 16,
+        "name": "Animation"
+    },
+    {
+        "id": 35,
+        "name": "Comedy"
+    },
+    {
+        "id": 80,
+        "name": "Crime"
+    },
+    {
+        "id": 99,
+        "name": "Documentary"
+    },
+    {
+        "id": 18,
+        "name": "Drama"
+    },
+    {
+        "id": 10751,
+        "name": "Family"
+    },
+    {
+        "id": 14,
+        "name": "Fantasy"
+    },
+    {
+        "id": 36,
+        "name": "History"
+    },
+    {
+        "id": 27,
+        "name": "Horror"
+    },
+    {
+        "id": 10402,
+        "name": "Music"
+    },
+    {
+        "id": 9648,
+        "name": "Mystery"
+    },
+    {
+        "id": 10749,
+        "name": "Romance"
+    },
+    {
+        "id": 878,
+        "name": "Science Fiction"
+    },
+    {
+        "id": 10770,
+        "name": "TV Movie"
+    },
+    {
+        "id": 53,
+        "name": "Thriller"
+    },
+    {
+        "id": 10752,
+        "name": "War"
+    },
+    {
+        "id": 37,
+        "name": "Western"
+    }
+];
+
+const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, genre_ids}, onClick }) => {
+    return (
+        <div className="movie-card" onClick={() => onClick?.()}>
+            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './no-poster.png'} alt={title} />
+            <h3 className="mt-4">{title}</h3>
+            <div className="content">
+                <div className="rating">
+                    <img src="./star.svg" alt="Rating"/>
+                    <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+                </div>
+                <p className="genre">{` • ${movieGenres.filter(genre => genre_ids.includes(genre.id)).map(genre => genre.name)[0]}`}</p>
+                <p className="year">{` • ${release_date ? release_date.split('-')[0] : "N/A"}`}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
---
+++ src/components/MovieDetailModal.jsx
@@ -0,0 +1,218 @@
+import React, {useState, useEffect} from 'react';
+import Spinner from "./Spinner.jsx";
+
+const MovieDetailModal = ({api:{API_BASE_URL, API_OPTIONS}, setIsModalOpen, movieId, onSelectRecommendedMovie}) => {
+    const [movieDetails, setMovieDetails] = useState({});
+    const [video, setVideo] = useState({});
+    const [recommendedMoviesList, setRecommendedMoviesList] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function formatNumber(num) {
+        if (num >= 1_000_000_000) {
+            return (num / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'B';
+        }
+        if (num >= 1_000_000) {
+            return (num / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
+        }
+        if (num >= 1_000) {
+            return (num / 1_000).toFixed(1).replace(/\.0$/, '') + 'K';
+        }
+        return num;
+    }
+
+    const fetchMovieDetails = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+        try{
+            const endpoint = `${API_BASE_URL}/movie/${movieId}`;
+            const response = await fetch(endpoint, API_OPTIONS);
+            if (!response.ok){
+                throw new Error('Failed to fetch movie details.');
+            }
+            const data = await response.json();
+            setMovieDetails(data || {});
+            if (data === 'False'){
+                setErrorMessage(data.error || "Failed to fetch movie details.");
+                return;
+            }
+        }
+        catch(error){
+            console.error(`Error: ${error.message}`)
+            setErrorMessage("Error fetching movies. Please try again later. ");
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    const fetchMovieVideo = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+        try{
+            const endpoint = `${API_BASE_URL}/movie/${movieId}/videos`;
+            const response = await fetch(endpoint, API_OPTIONS);
+            if (!response.ok){
+                throw new Error('Failed to fetch movie videos.');
+            }
+            const data = await response.json();
+            setVideo(data.results.find(v => v.site === 'YouTube' && v.type === 'Trailer' && v.official) || {})
+            if (data === 'False'){
+                setErrorMessage(data.error || "Failed to fetch movie videos.");
+                return;
+            }
+        }
+        catch(error){
+            console.error(`Error: ${error.message}`)
+            setErrorMessage("Error fetching movies. Please try again later. ");
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    const getRecommendedMovies = async() => {
+        setIsLoading(true);
+        setErrorMessage('');
+        try {
+            const endpoint = `${API_BASE_URL}/movie/${movie.id}/recommendations`;
+            const response = await fetch(endpoint, API_OPTIONS);
+            if (!response.ok){
+                throw new Error('Failed to fetch recommended movies.');
+            }
+            const data = await response.json();
+            setRecommendedMoviesList(data.results || []);
+            if (data.response === 'False'){
+                setErrorMessage(data.error || "Failed to fetch recommended movies.");
+                setRecommendedMoviesList(data.results || []);
+                return;
+            }
+        }
+        catch(error){
+                console.error(`Error: ${error.message}`)
+                setErrorMessage("Error fetching movies. Please try again later. ");
+            }
+        finally {
+                setIsLoading(false);
+            }
+    }
+
+    useEffect(() => {
+        if (movieId) {
+            fetchMovieDetails();
+            fetchMovieVideo();
+            getRecommendedMovies();
+        }
+    }, [movieId]);
+    console.log('movieDetails', movieDetails)
+
+    const movieData = [
+        {
+            title: "Genre",
+            value:  movieDetails?.genres?.map(g => g.name)
+        },
+        {
+            title: "Overview",
+            value:  movieDetails?.overview
+        },
+        {
+            title: "Release date",
+            value:  `${movieDetails?.release_date?.split('-')[0]} • ${Math.trunc(movieDetails?.runtime/60)}h ${movieDetails?.runtime%60}min`
+        },
+        {
+            title: "Countries",
+            value: movieDetails?.production_countries?.map(c => c.name)
+        },
+        {
+            title: "Status",
+            value: movieDetails?.status
+        },
+        {
+            title: "Spoken languages",
+            value: movieDetails?.spoken_languages?.map(l => l.english_name)
+        },
+        {
+            title: "Budget",
+            value:  `$${formatNumber(movieDetails?.budget)}`
+        },
+        {
+            title: "Revenue",
+            value:  `$${formatNumber(movieDetails?.revenue)}`
+        },
+        {
+            title: "Tagline",
+            value:  movieDetails?.tagline
+        },
+        {
+            title: "Production companies",
+            value: movieDetails?.production_companies?.map(c => c.name)
+        }
+    ]
+
+    return (
+        <div className="movie-detail">
+            <section className="movie-information">
+                <img src="/close.svg" alt="Close" onClick={() => setIsModalOpen(false)} className="close-icon"/>
+                <div className="movie-header">
+                    <h2>{movieDetails.title} {movieDetails.title !== movieDetails.original_title && ` - ${movieDetails.original_title}`}</h2>
+                    <p className="btn-style"><img src='/star.svg' alt="star"/> {movieDetails?.vote_average?.toFixed(1)}/10 ({formatNumber(movieDetails?.vote_count)})</p>
+                </div>
+                <p>{`${movieDetails?.release_date?.split('-')[0]} • ${Math.trunc(movieDetails?.runtime/60)}h ${movieDetails?.runtime%60}min`}</p>
+                <div className="movie-poster">
+                    <img src={movieDetails.poster_path ? `https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}` : './no-poster.png'} alt={movieDetails.title} />
+                    {
+                        video && (
+                            <iframe
+
+                                src={`https://www.youtube.com/embed/${video.key}`}
+                                title={movieDetails?.title}
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        )
+                    }
+                </div>
+                <div className="movie-description">
+                    {
+                        movieData.map(data => (
+                            <div className="movie-data" key={data.title}>
+                                <h3 className="description-title">{data.title}</h3>
+                                {
+                                    Array.isArray(data.value)
+                                    ? (
+                                        data.title === "Genre" ? (
+                                            <div className="flex gap-2 flex-wrap">
+                                                {data.value.map(genre => <p key={genre} className="btn-style">{genre}</p>)}
+                                            </div>
+                                        ) : <p className="description-text">{data.value.join('• ')}</p>
+                                      ) : data.value
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
+            </section>
+            {/*{*/}
+            {/*    isLoading ? <Spinner/> :*/}
+            {/*    errorMessage ? <p className="text-red-500 ">{errorMessage}</p> :*/}
+            {/*    <section className="recommended">*/}
+            {/*        <h2>Recommendations</h2>*/}
+            {/*        <ul>*/}
+            {/*            {*/}
+            {/*                recommendedMoviesList.length > 0 && (*/}
+            {/*                    recommendedMoviesList.map(movie => (*/}
+            {/*                        <li key={movie.id} onClick={() => onSelectRecommendedMovie(movie)}>*/}
+            {/*                            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}/>*/}
+            {/*                            <h3>{movie.title}</h3>*/}
+            {/*                        </li>*/}
+            {/*                    ))*/}
+            {/*                )*/}
+            {/*            }*/}
+            {/*        </ul>*/}
+            {/*    </section>*/}
+            {/*}*/}
+        </div>
+    )
+}
+
+export default MovieDetailModal;
